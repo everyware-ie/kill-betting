f5bcf91928163254a8a9c8fc67925b89e5e968fb
‘jminkkk’ <102847513+jminkkk@users.noreply.github.com>
2026-04-07 14:47:48 +0900

    refactor(config): ResourceHandlerRegistry 의도를 명확하게 수정

--- backend/src/main/java/com/killnagi/config/FileServingConfig.java
+++ backend/src/main/java/com/killnagi/config/FileServingConfig.java
@@ -6,7 +6,7 @@ import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class WebMvcConfig implements WebMvcConfigurer {
+public class FileServingConfig implements WebMvcConfigurer {
 
     @Value("${file.upload-dir:uploads}")
     private String uploadDir;
